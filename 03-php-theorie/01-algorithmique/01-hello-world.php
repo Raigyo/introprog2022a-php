c9800bd9c215e16590421b9c5822ad5b0efa61c5
chilot <https://github.com/Raigyo>
2022-02-24 13:58:52 +0100

    first commit

---
+++ 03-php-theorie/01-algorithmique/01-hello-world.php
@@ -0,0 +1,15 @@
+<h1>Hello world!</h1>
+
+<!-- On introduit du code php par la balise php / pas obligatoire de la fermer -->
+<?php
+  // instruction d'affichage, le serveur interprète le code
+  echo "Je suis du PHP";
+  echo "<p><strong>Je suis du PHP + HTML</strong></p>";
+
+  // pas interprété: erreur de syntaxe
+  // Hello world
+  // <h1>Hello world</h1>
+?>
+
+<!-- HTML: interprété automatiquement par le navigateur -->
+<h1>Je suis du HTML</h1>
