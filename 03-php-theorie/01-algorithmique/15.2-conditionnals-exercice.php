e49b96567979c189d350d090ef83e023b509af0b
Vincent Chilot <vincent.chilot@gmail.com>
2022-03-03 10:29:47 +0100

    exercice conditionnal

--- 03-php-theorie/01-algorithmique/15.2-conditionnals-exercice.php
+++ 03-php-theorie/01-algorithmique/15.2-conditionnals-exercice.php
@@ -9,24 +9,7 @@
   // Récupérer variables age en paramètre
   // Créer une nouvelle variable résultat
   // Faire le calcul de la différence (attention au résulat négatif: if)
+  // Retourner le resulat depuis la fonction
   // Afficher le résulat
-
-  $personName1 = "Frodon";
-  $personAge1 = 33;
-
-  $personName2 = "Sam";
-  $personAge2 = 21;
-
-  function calculateAgeDifference ($personAge1, $personAge2) {
-    $result = $personAge1 - $personAge2;
-    if($result < 0) {
-      $result = -$result;
-    }
-    return $result;
-  }
-
-  // echo calculateAgeDifference ($personAge1, $personAge2);
-  $ageDifference = calculateAgeDifference ($personAge1, $personAge2);
-  echo "La différence d'âge est de " . $ageDifference;
 
 ?>
